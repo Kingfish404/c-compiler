341fc62866d5cde7e5eb146c6e51aff4144ce770
Kingfish404 <jinyu929@qq.com>
2021-06-11 14:47:52 +0800

    feat: add c final,etc

---
+++ src/c-interperter.c
@@ -0,0 +1,11 @@
+//
+// Created by Jin Yu on 2021/2/15.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello");
+    return 0;
+}
---
+++ src/helloworld.c
@@ -0,0 +1,4 @@
+//
+// Created by Jin Yu on 2021/2/15.
+//
+
